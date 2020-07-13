c5eff6c0015d429826c0e2ee7df85ceaa9b21315
reuven9632 <reuven9632@gmail.com>
2020-07-13 23:49:44 +0300

    make GUI

---
+++ src/com/company/SimpleGUI.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;//библиотека отслеживания нажатия на кнопки
+import javax.swing.*;//библиотека для созданитя формочек: надпись, кнопка, поле и ...
+
+
+
+public class SimpleGUI extends JFrame{ //наследуем класс JFrame для созданитя формочек: надпись, кнопка, поле и ...
+
+}
